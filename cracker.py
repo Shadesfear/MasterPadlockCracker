7e6b63eca995c5ff96074910d34fa6a596b3952b
DESKTOP-HVP6MPC\Chris <chris@thecarmans.com>
2017-05-09 14:19:41 +0200

    the python master padlock cracker

---
+++ cracker.py
@@ -0,0 +1,54 @@
+import math
+import os
+def combo():
+	tmp=1
+	first = (math.ceil(resLoc)+5)%40
+	second = []
+	third = []
+
+	mod = first%4
+
+	for i in range(0,4):
+		if ((10*i)+firstDigit)%4 == mod:
+			third.append((10*i)+firstDigit)
+
+		if ((10*i)+secondDigit)%4 == mod:
+			third.append((10*i)+secondDigit)
+	#print(first)
+	ty = int(input('Is the third digit ' + str(third[0]) + ' or is it ' + str(third[1])+' ?:'))
+
+	if ty == third[0]:
+		third=ty
+		for i in range(0,10):
+			tmp = ((mod+2)%4)+(4*i)
+			if (third+2)%40 != tmp and (third-2)%40 != tmp:
+				second.append(tmp)
+
+	elif ty == third[1]:
+		third = ty
+		for i in range(0,10):
+			tmp = ((mod-2)%4)+(4*i)
+			if (third+2)%40 != tmp and (third-2)%40 != tmp:
+				second.append(tmp)
+
+	os.system('cls' if os.name =='nt' else clear)
+	print('======================================')
+	print('You can now crack your master padlock!')
+	print('======================================\n')
+
+	print('Your first digit is: '+str(first)+'\n')
+	print('Your second digits are: ')
+	print(*second)
+	print('\n')
+	print('Your third digit is: '+str(third))
+
+firstDigit = int(input('Enter first digit: '))
+secondDigit = int(input('Enter second Digit: '))
+resLoc = int(input('Enter resistant location: '))
+
+if firstDigit and secondDigit and resLoc:
+	combo()
+
+
+
+
