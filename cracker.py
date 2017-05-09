f8127da37843fdf156ffd6b63c76d703565f14e8
DESKTOP-HVP6MPC\Chris <chris@thecarmans.com>
2017-05-09 15:08:25 +0200

    Fixed up some shitty code

--- cracker.py
+++ cracker.py
@@ -4,7 +4,6 @@ def combo():
 	first = (math.ceil(resLoc)+5)%40
 	second = []
 	third = []
-
 	mod = first%4
 
 	for i in range(0,4):
@@ -13,33 +12,23 @@ def combo():
 
 		if ((10*i)+secondDigit)%4 == mod:
 			third.append((10*i)+secondDigit)
-	#print(first)
+
 	ty = int(input('Is the third digit ' + str(third[0]) + ' or is it ' + str(third[1])+' ?:'))
-
-	if ty == third[0]:
-		third=ty
-		for i in range(0,10):
-			tmp = ((mod+2)%4)+(4*i)
-			if (third+2)%40 != tmp and (third-2)%40 != tmp:
-				second.append(tmp)
-
-	elif ty == third[1]:
-		third = ty
-		for i in range(0,10):
-			tmp = ((mod-2)%4)+(4*i)
-			if (third+2)%40 != tmp and (third-2)%40 != tmp:
-				second.append(tmp)
+	for i in range(0,10):
+		tmp = ((mod+2)%4)+(4*i)
+		if (ty+2)%40 != tmp and (ty-2)%40 != tmp:
+			second.append(tmp) 
+	second.append(tmp)
 
 	os.system('cls' if os.name =='nt' else clear)
 	print('======================================')
 	print('You can now crack your master padlock!')
 	print('======================================\n')
-
 	print('Your first digit is: '+str(first)+'\n')
 	print('Your second digits are: ')
 	print(*second)
 	print('\n')
-	print('Your third digit is: '+str(third))
+	print('Your third digit is: '+str(ty))
 
 firstDigit = int(input('Enter first digit: '))
 secondDigit = int(input('Enter second Digit: '))
